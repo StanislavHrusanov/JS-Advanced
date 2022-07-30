8d8f9fd00fdcbbdac8c1d54353e9e6b2bc8d2705
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-07-30 14:36:02 +0300

    Add problem solution

---
+++ Syntax Functions and Statements/Lab/CircleArea.js
@@ -0,0 +1,9 @@
+function circleArea(r) {
+    if (typeof (r) == 'number') {
+        let area = r ** 2 * Math.PI;
+        console.log(area.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof (r)}.`);
+    }
+}
+circleArea(`We can not calculate the circle area, because we receive a {type of argument}.`);
