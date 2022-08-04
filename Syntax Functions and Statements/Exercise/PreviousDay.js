823497410554e3ecfb6938294baee12486b63745
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-08-04 13:29:59 +0300

    Add problem solution

---
+++ Syntax Functions and Statements/Exercise/PreviousDay.js
@@ -0,0 +1,5 @@
+function previousDay(year, month, day) {
+    let date = new Date(year, month - 1, day - 1);
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+}
+previousDay(2020, 3, 1);
