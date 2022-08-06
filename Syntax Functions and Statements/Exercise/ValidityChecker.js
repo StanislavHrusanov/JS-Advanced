d18bc3302cbf57e5c547b0d608fc569412f06543
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-08-06 15:12:31 +0300

    Add problem solution

---
+++ Syntax Functions and Statements/Exercise/ValidityChecker.js
@@ -0,0 +1,18 @@
+function validityChecker(x1, y1, x2, y2) {
+
+    console.log(`{${x1}, ${y1}} to {0, 0} ${checkValidity(x1, y1, 0, 0)}`);
+    console.log(`{${x2}, ${y2}} to {0, 0} ${checkValidity(x2, y2, 0, 0)}`);
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} ${checkValidity(x1, y1, x2, y2)}`);
+
+    function checkValidity(x1, y1, x2, y2) {
+
+        let distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+        if (Number.isInteger(distance)) {
+            return 'is valid';
+        } else {
+            return 'is invalid';
+        }
+    }
+}
+validityChecker(3, 0, 0, 4);
