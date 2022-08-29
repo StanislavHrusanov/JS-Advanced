3c723e764be152b02b3a8cdb697907d4f0c39759
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-08-29 15:58:11 +0300

    Add JS file

---
+++ DOM Introduction/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let firstOperand = Number(document.getElementById('num1').value);
+    let secOperand = Number(document.getElementById('num2').value);
+
+    return document.getElementById('sum').value = firstOperand + secOperand;
+}
