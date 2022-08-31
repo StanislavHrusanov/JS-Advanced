1ae4999414a334edc37d0d3517ccc9607a5dff8b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-08-31 11:58:07 +0300

    Add JS file

---
+++ DOM Introduction - Exercise/accordion.js
@@ -0,0 +1,10 @@
+function toggle() {
+    if (document.getElementsByClassName('button')[0].textContent == 'More') {
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+        document.getElementById('extra').style.display = 'block';
+
+    } else if (document.getElementsByClassName('button')[0].textContent == 'Less') {
+        document.getElementsByClassName('button')[0].textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
+}
