a7e0fc4ee660ccd759416d0c9dd934417f1772d2
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-09-20 12:50:17 +0300

    Delete folder

--- DOM Introduction - Exercise/Subtraction/Subtraction.js
+++
@@ -1,6 +0,0 @@
-function subtract() {
-    let firstNum = document.getElementById('firstNumber').value;
-    let secondNum = document.getElementById('secondNumber').value;
-
-    console.log(document.getElementById('result').textContent = Number(firstNum) - Number(secondNum));
-}
