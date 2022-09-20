d0c3a4ceb33231d68780e1a6d3892bfa25af41cf
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-09-20 12:52:00 +0300

    Add JS file to folder

---
+++ DOM Introduction - Exercise/Subtraction/Subtraction.js
@@ -0,0 +1,6 @@
+function subtract() {
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+
+    console.log(document.getElementById('result').textContent = Number(firstNum) - Number(secondNum));
+}
