41c5b125013036bea815f39aa8cea5a60f8c3ae8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-11-20 12:06:50 +0200

    Add SubSum.js

---
+++ Unit Testing and Error Handling/Lab/SubSum.js
@@ -0,0 +1,13 @@
+function subSum(arr, startIndex, endIndex) {
+    if (!Array.isArray(arr)) {
+        return NaN;
+    }
+    if (startIndex < 0) {
+        startIndex = 0;
+    }
+    if (endIndex > arr.length - 1) {
+        endIndex = arr.length - 1;
+    }
+
+    return arr.slice(startIndex, endIndex + 1).reduce((a, b) => Number(a) + Number(b), 0);
+}
