5d2c8533a8d3e3899303d608fd39d8b22f0ad5b1
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-12-04 11:31:23 +0200

    Add person.js

---
+++ Prototypes and Inheritance/Lab/person.js
@@ -0,0 +1,26 @@
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(value) {
+        const [firstN, lastN] = value.split(' ');
+
+        if (firstN && lastN) {
+            this.firstName = firstN;
+            this.lastName = lastN;
+        }
+    }
+}
+let person = new Person("Albert", "Simpson");
+console.log(person.fullName); //Albert Simpson
+person.firstName = "Simon";
+console.log(person.fullName); //Simon Simpson
+person.fullName = "Peter";
+console.log(person.firstName);  // Simon
+console.log(person.lastName);  // Simpson
