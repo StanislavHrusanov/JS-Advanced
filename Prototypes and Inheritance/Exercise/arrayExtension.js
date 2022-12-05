e905235b460285a6f47db7900ffca86eada40c46
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-12-05 14:36:47 +0200

    Add arrayExtension.js

---
+++ Prototypes and Inheritance/Exercise/arrayExtension.js
@@ -0,0 +1,26 @@
+(function solve() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    }
+
+    Array.prototype.skip = function (n) {
+        return this.slice(n);
+    }
+
+    Array.prototype.take = function (n) {
+        return this.slice(0, n);
+    }
+
+    Array.prototype.sum = function () {
+        return this.reduce((a, b) => a + b, 0);
+    }
+
+    Array.prototype.average = function () {
+        let sum = 0;
+        for (let num of this) {
+            sum += num;
+        }
+        let avg = sum / this.length;
+        return avg;
+    }
+})();
